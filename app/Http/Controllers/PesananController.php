18d43265ecdd9e7bcf097dbc7ec9e8ae48305742
rifkigantengsangat <radenmuhamadrifki@gmail.com>
2022-09-04 23:46:00 +0700

    create Login Register with Token

---
+++ app/Http/Controllers/PesananController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pesanan;
+use Illuminate\Http\Request;
+
+class PesananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\pesanan  $pesanan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pesanan $pesanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\pesanan  $pesanan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pesanan $pesanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\pesanan  $pesanan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pesanan $pesanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\pesanan  $pesanan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pesanan $pesanan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Http\Controllers\api;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller 
+{
+    public function createUser(Request $request)
+    {
+      try {
+        $validatedUser = Validator::make($request->all(),[
+            'name'=> 'required',
+            'email'=> 'required | unique:users,email',
+            'password'=> 'required|min:8 |max:25',
+        ]);
+        if($validatedUser->fails()){
+            return response()->json([
+               'errors'=>$validatedUser->errors(),
+               'status'=>401,
+               'message'=>'validation errors' 
+            ],401);   
+        }
+         $user = new User;
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=Hash::make($request->password);
+        $user->save();
+      $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+               'status'=>201,
+               'message'=>'User created successfully',
+               'token' => $token
+        ],201);
+      } catch (\Throwable $th) {
+        return response()->json([
+            'status'=>500,
+            'message'=> $th->getMessage()
+        ],500);
+      }
+    }
+    public function loginUser(Request $request)
+    {
+        try {
+            $validatedUser = Validator::make($request->all(), 
+            [
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+            if($validatedUser->fails()){
+                return response()->json([
+                    'status'=>401,
+                    'message' => 'Validation Errors',
+                    'error'=>$validatedUser->errors(),
+                ],401);
+            
+            }
+            if(!Auth::attempt($request->only('email','password'))){
+                return response()->json([
+                    'status'=>401,
+                    'message' =>'email and password are not Valid',
+                ],401);
+            }
+            $user = User::where('email',$request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'status'=>200,
+                'message' =>'success Login Successfully',
+                'data'=>$user,
+                'token' =>$token,
+                'token_type'=>'Bearer'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status'=>500,
+                'error'=>$th.getMessage()
+
+            ],500);
+        }
+           
+        
+        
+       
+    }
+}
